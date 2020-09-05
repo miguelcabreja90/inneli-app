99906e91e76d9ca482c757acdd3a5ea24ac7af6f
Miguel Antonio <miguel.cabreja90@gmail.com>
2020-09-04 23:35:23 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+## How to use
+
+- Clone the repository with __git clone__
+
+__Back-end__
+
+- Go to the sub-folder `cd backend`
+- Copy __.env.example__ file to __.env__ and edit database credentials there
+- Add correct value of your domain for front URL in this variable: `FRONTPAGE_VERIFY_URL=[your_domain]/verify/`
+- Run __composer install__
+- Run __php artisan key:generate__
+- Run __php artisan migrate --seed__ (it has some seeded data for your testing)
+- Run __php artisan passport:install__
+- Run __php artisan serve__
+
+
+__Front-end__
+
+- Go to the sub-folder `cd fronted`
+- Copy __.env.local.example__ to __.env.local__ and edit URL of your API
+- Run __npm install__ 
+- Run __npm run serve__ 
+- That's it: launch the URL that appears in the terminal - could be `http://[your_domain]`
+
+
+## License
+
+MIT
+
+---
---
+++ fronted/src/views/Home.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="home mt-5">
+    <div v-if="success" class="alert alert-success" role="alert">
+      {{ success }}
+    </div>
+    <div v-if="error" class="alert alert-danger" role="alert">
+      {{ error }}
+    </div>
+    <h2 v-if="!user">Welcome, please log in or register</h2>
+    <h2 v-else-if="!user.email_verified_at">
+      Hello, {{ user.name }}! Registration successful, please check your inbox
+      and click confirmation link. If you did not receive the email, click
+      <a href="#" @click="verifyResend">here</a> to request another
+    </h2>
+    <h2 v-else>Hello, {{ user.name }}! You're in.</h2>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+
+export default {
+  name: "Home",
+
+  data() {
+    return {
+      success: null,
+      error: null
+    };
+  },
+
+  computed: {
+    ...mapGetters("auth", ["user"])
+  },
+
+  methods: {
+    ...mapActions("auth", ["sendVerifyResendRequest"]),
+
+    verifyResend() {
+      this.success = this.error = null;
+      this.sendVerifyResendRequest()
+          .then(() => {
+            this.success =
+                "A fresh verification link has been sent to your email address.";
+          })
+          .catch(error => {
+            this.error = "Error sending verification link.";
+            console.log(error.response);
+          });
+    }
+  }
+};
+</script>
