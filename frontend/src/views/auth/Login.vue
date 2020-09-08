aa08558bbcd59937a425b10ef67cce6a7896868e
Miguel Antonio <miguel.cabreja90@gmail.com>
2020-09-08 13:12:27 -0400

    first commit

--- frontend/src/views/auth/Login.vue
+++ frontend/src/views/auth/Login.vue
@@ -2,24 +2,27 @@
   <v-card class="elevation-1 pa-3 login-card">
     <v-card-text>
       <div class="layout column align-center">
-        <img src="/static/m.png" alt="Vue Material Admin" width="120" height="120" />
+        <img src="/static/m.png" alt="Vue Material Admin" width="120" height="120"/>
         <h1 class="flex my-4 primary--text">{{ $t("login.title") }}</h1>
       </div>
-      <v-form>
+      <v-form v-model="valid" ref="form">
         <v-text-field
           append-icon="email"
           name="login"
           :label="$t('login.account')"
           type="email"
           v-model="details.email"
+          :rules="[rules.required]"
         ></v-text-field>
         <v-text-field
-          append-icon="lock"
+          :type="hidePassword ? 'password' : 'text'"
+          :append-icon="hidePassword ? 'fa fa-eye' : 'fa fa-low-vision'"
           name="password"
           :label="$t('login.password')"
           id="password"
-          type="password"
           v-model="details.password"
+          @click:append="hidePassword = !hidePassword"
+          :rules="[rules.required]"
         ></v-text-field>
       </v-form>
     </v-card-text>
@@ -44,22 +47,31 @@
 import { mapActions, mapState } from "vuex"
 
 export default {
-  data: () => ({
-    dialog: false,
-    details: {
-      email: null,
-      password: null
+  data() {
+    return {
+      dialog: false,
+      details: {
+        email: null,
+        password: null
+      },
+      valid: false,
+      hidePassword: true,
+      rules: {
+        required: v => !!v || this.$t("rules.required")
+      }
     }
-  }),
+  },
   computed: {
     ...mapState("auth", ["userData", "isLoggedIn", "pending"])
   },
   methods: {
     ...mapActions("auth", ["sendLoginRequest"]),
     login: function() {
-      this.sendLoginRequest(this.details).then(() => {
-        this.$router.push({ name: "Dashboard" })
-      })
+      if (this.$refs.form.validate()) {
+        this.sendLoginRequest(this.details).then(() => {
+          this.$router.push({ name: "Dashboard" })
+        })
+      }
     }
   }
 }
