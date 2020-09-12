316443097c42c096a8949c72032d6f5d2832b524
Miguel Antonio <miguel.cabreja90@gmail.com>
2020-09-12 03:33:49 -0400

    first commit

--- frontend/src/views/Welcome.vue
+++ frontend/src/views/Welcome.vue
@@ -3,22 +3,12 @@
     <v-alert prominent color="orange" style="margin: 20px" icon="mdi-alert">
       <v-row align="center">
         <v-col class="grow">
-          {{ $vuetify.lang.t('$vuetify.activeAccount')}}
+          {{ $vuetify.lang.t('$vuetify.activeAccount') }}
         </v-col>
       </v-row>
     </v-alert>
     <v-card class="pa-3 page-login__card" tile>
-      <v-img src="/static/avatar/avatar-undefined.jpg" height="300">
-        <v-layout column class="media ma-0">
-          <v-card-title>
-            <v-spacer></v-spacer>
-            <v-btn dark icon class="mr-3">
-              <v-icon>edit</v-icon>
-            </v-btn>
-          </v-card-title>
-          <v-spacer></v-spacer>
-        </v-layout>
-      </v-img>
+      <v-img src="/static/avatar/avatar-undefined.jpg" height="300"> </v-img>
       <v-list two-line class="pa-0">
         <v-list-item href="#">
           <v-list-item-action>
@@ -26,8 +16,15 @@
           </v-list-item-action>
           <v-list-item-content>
             <v-list-item-title
-              v-text="userData.firstName?userData.firstName:$vuetify.lang.t('$vuetify.noDefined')"></v-list-item-title>
-            <v-list-item-subtitle v-text="$vuetify.lang.t('$vuetify.firstName')"></v-list-item-subtitle>
+              v-text="
+                userData.firstName
+                  ? userData.firstName
+                  : $vuetify.lang.t('$vuetify.noDefined')
+              "
+            ></v-list-item-title>
+            <v-list-item-subtitle
+              v-text="$vuetify.lang.t('$vuetify.firstName')"
+            ></v-list-item-subtitle>
           </v-list-item-content>
         </v-list-item>
         <v-divider inset></v-divider>
@@ -37,8 +34,15 @@
           </v-list-item-action>
           <v-list-item-content>
             <v-list-item-title
-              v-text="userData.email?userData.email:$vuetify.lang.t('$vuetify.noDefined')"></v-list-item-title>
-            <v-list-item-subtitle v-text="$vuetify.lang.t('$vuetify.email')"></v-list-item-subtitle>
+              v-text="
+                userData.email
+                  ? userData.email
+                  : $vuetify.lang.t('$vuetify.noDefined')
+              "
+            ></v-list-item-title>
+            <v-list-item-subtitle
+              v-text="$vuetify.lang.t('$vuetify.email')"
+            ></v-list-item-subtitle>
           </v-list-item-content>
         </v-list-item>
       </v-list>
@@ -48,29 +52,29 @@
 </template>
 
 <script>
-  import {mapState, mapActions, mapGetters} from 'vuex'
+import { mapActions, mapGetters, mapState } from 'vuex'
 
-  export default {
-    name: 'Welcome',
-    computed: {
-      ...mapState('auth', ['isLoggedIn', 'userData']),
-      ...mapGetters(['errors']),
-    },
-    mounted() {
-      this.getUserData()
-    },
-    created() {
-      this.getUserData()
-    },
-    methods: {
-      ...mapActions('auth', ['getUserData'])
-    }
+export default {
+  name: 'Welcome',
+  computed: {
+    ...mapState('auth', ['isLoggedIn', 'userData']),
+    ...mapGetters(['errors'])
+  },
+  mounted() {
+    this.getUserData()
+  },
+  created() {
+    this.getUserData()
+  },
+  methods: {
+    ...mapActions('auth', ['getUserData'])
   }
+}
 </script>
 
 <style lang="sass" scoped>
-  .page-login
-    &__card
-      max-width: 600px
-      margin: 0 auto
+.page-login
+  &__card
+    max-width: 600px
+    margin: 0 auto
 </style>
